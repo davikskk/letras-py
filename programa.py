2cb40e675cce025d1cdeba9180a4e0aa178845e9
davikskk <davimasc33@gmail.com>
2026-05-20 14:47:43 -0300

    letras

--- programa.py
+++ programa.py
@@ -4,8 +4,9 @@ lista=[]
 #ex: lista=[("letra da música", tempo)]
 while len(lista)>0:
     os.system('clear')
-    print("Impostor Syndrome")
-    print("\033[0;31mSidney Gish\033[m\n")
+    #substitua
+    print("Nome da música")
+    print("\033[0;31m Cantor da música \033[m\n")
     for i,(texto, tempo) in enumerate(lista):
         if i==0:
             print(texto)
