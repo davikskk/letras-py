c914fd69a253e0ca2f60e5d43851ef8817c44fc5
davikskk <davimasc33@gmail.com>
2026-05-20 12:03:38 -0300

    letras

---
+++ programa.py
@@ -0,0 +1,16 @@
+import time, os
+lista=[]
+#coloque na lista a letra de uma música e o tempo de espera
+#ex: lista=[("letra da música", tempo)]
+while len(lista)>0:
+    os.system('clear')
+    print("Impostor Syndrome")
+    print("\033[0;31mSidney Gish\033[m\n")
+    for i,(texto, tempo) in enumerate(lista):
+        if i==0:
+            print(texto)
+        else:
+            print(f"\033[0;90m{texto}\033[m")
+    time.sleep(lista[0][1])
+    del lista[0]
+os.system('clear')
